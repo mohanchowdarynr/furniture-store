8e46878038cc26373c96d85e73c4b60fbcff0173
mohan nr <44423216+mohanchowdarynr@users.noreply.github.com>
2021-07-27 12:04:54 +0530

    Done with basic Navbar

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from './Home';
+import About from './About';
+import Checkout from './Checkout';
+import Cart from './Cart';
+import Error from './Error';
+import Products from './Products';
+import SingleProduct from './SingleProduct';
+import PrivateRoute from './PrivateRoute';
+
+export {Home,About,Cart,Checkout,Error,SingleProduct,Products,PrivateRoute};
